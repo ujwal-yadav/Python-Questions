3d4a782240e870c39921552d88d8a30f632406f8
Ujwal Kumar Yadav <87336302+ujwal-yadav@users.noreply.github.com>
2022-01-11 20:16:07 +0530

    Create Concatenationofstrings.py

---
+++ Concatenationofstrings.py
@@ -0,0 +1,18 @@
+s="3,2,6,5,1,4,8,9"
+s=s.split(",")
+num1=0
+num2=""
+i=0;
+while i<len(s) :
+    if(s[i]=="5"):
+        while(s[i]!="8"):
+            num2=num2+s[i]
+            i=i+1;
+        if(s[i]=="8"):
+            num2=num2+s[i]
+            i=i+1;
+    else:
+        num1=num1+int(s[i])
+        i=i+1;
+
+print(num1+int(num2))
