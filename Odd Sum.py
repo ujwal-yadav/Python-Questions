f27ec187a68285d6b3ef407247eb6e046e33d59e
Ujwal Kumar Yadav <ujwalofficial99@gmail.com>
2022-03-14 02:16:00 +0530

    Update Odd Sum.py

--- Odd Sum.py
+++ Odd Sum.py
@@ -1,12 +1,12 @@
 import itertools
 arr=list(map(int,input().split(" ")))
 arr=sorted(arr)
-m=0
+ma=0
 
 for i in range(len(arr)):
     lst=list(itertools.combinations(arr,len(arr)-i))
     for j in lst:
         s=sum(j)
         if s%2!=0:
-            m=max(m,s)
-print(m)
+            ma=max(ma,s)
+print(ma)
