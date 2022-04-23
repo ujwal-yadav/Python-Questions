742a2eeab63a6886092d671d4e75e00690f6f5b5
Ujwal Kumar Yadav <ujwalofficial99@gmail.com>
2022-04-23 15:44:12 +0530

    Update spiral matrix 2.py

--- spiral matrix 2.py
+++ spiral matrix 2.py
@@ -1,31 +1,32 @@
-def generateMatrix(self, n: int) -> List[List[int]]:
-   mat=[[0 for i in range(n)]for j in range(n)]
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        mat=[[0 for i in range(n)]for j in range(n)]
         
-   t,b=0,n-1
-   l,r=0,n-1
-   ele=1
+        t,b=0,n-1
+        l,r=0,n-1
+        ele=1
         
-   while (t<=b and l<=r):
-       for i in range(l,r):
-           mat[t][i]=ele
-           ele+=1
+        while (t<=b and l<=r):
+            for i in range(l,r+1):
+                mat[t][i]=ele
+                ele+=1
                 
-       for i in range(t,b):
-           mat[i][r]=ele
-           ele+=1
+            for i in range(t+1,b+1):
+                mat[i][r]=ele
+                ele+=1
                 
-       for i in reversed(range(l,r+1)):
-           mat[b][i]=ele
-           ele+=1
+            for i in reversed(range(l,r)):
+                mat[b][i]=ele
+                ele+=1
                 
-       for i in reversed(range(t+1,b)):
-           mat[i][l]=ele
-           ele+=1
+            for i in reversed(range(t+1,b)):
+                mat[i][l]=ele
+                ele+=1
             
-       t+=1
-       b-=1
-       l+=1
-       r-=1
+            t+=1
+            b-=1
+            l+=1
+            r-=1
             
-   return mat
-   
+        return mat
+        
