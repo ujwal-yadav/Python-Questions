6d5a86168076738961c56a92a791a29bf8c3be39
Ujwal Kumar Yadav <ujwalofficial99@gmail.com>
2022-05-07 02:11:27 +0530

    Update and rename kadane's algo (Max Subarray).py to kadane's algo(Max sum Subarray).py

--- kadane's algo(Max sum Subarray).py
+++ kadane's algo(Max sum Subarray).py
@@ -1,10 +1,19 @@
 def maxSubArray(self, nums: List[int]) -> int:
     summ=0
+    c=0
+    temp=[]
     maxx=nums[0]
     for i in range(len(nums)):
         summ+=nums[i]
         if summ>maxx:
+            temp=[]
             maxx=summ
+            temp.append(c)
+            temp.append(i)
         if summ<0:
             summ=0
+            c=i+1
+    for i in range(temp[0],temp[1]+1):
+        print(nums[i],end=" ")
     return maxx
+                
